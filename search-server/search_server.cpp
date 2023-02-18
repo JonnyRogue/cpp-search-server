baade737f94d2045aecbb9246005a22bc55ad083
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-02-18 17:08:41 +0300

    Update search_server.cpp

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -14,26 +14,26 @@ void SearchServer:: AddDocument(int document_id, const std::string& document, Do
         }
         documents_.emplace(document_id, DocumentData{ComputeAverageRating(ratings), status});
         document_ids_.push_back(document_id);
-  }
+}
 
 std::vector<Document> SearchServer:: FindTopDocuments(const std::string& raw_query, DocumentStatus status) const {
         return FindTopDocuments(
             raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
                 return document_status == status;
             });
-  }
+}
 
 std::vector<Document> SearchServer:: FindTopDocuments(const std::string& raw_query) const {
         return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
-  }
+}
 
 int SearchServer:: GetDocumentCount() const {
         return documents_.size();
-  }
+}
 
 int SearchServer:: GetDocumentId(int index) const {
         return document_ids_.at(index);
-  }
+}
 
 std::tuple<std::vector<std::string>, DocumentStatus> SearchServer:: MatchDocument(const std::string& raw_query,
                                                         int document_id) const {
@@ -58,18 +58,18 @@ std::tuple<std::vector<std::string>, DocumentStatus> SearchServer:: MatchDocumen
             }
         }
         return {matched_words, documents_.at(document_id).status};
-  }
+}
 
 bool SearchServer:: IsStopWord(const std::string& word) const {
         return stop_words_.count(word) > 0;
-  }
+}
 
 bool SearchServer:: IsValidWord(const std::string& word) {
         // A valid word must not contain special characters
         return none_of(word.begin(), word.end(), [](char c) {
             return c >= '\0' && c < ' ';
         });
-  }
+}
 
 std::vector<std::string> SearchServer:: SplitIntoWordsNoStop(const std::string& text) const {
         std::vector<std::string> words;
@@ -82,7 +82,7 @@ std::vector<std::string> SearchServer:: SplitIntoWordsNoStop(const std::string& 
             }
         }
         return words;
-  }
+}
 
 int SearchServer:: ComputeAverageRating(const std::vector<int>& ratings) {
         if (ratings.empty()) {
@@ -90,7 +90,7 @@ int SearchServer:: ComputeAverageRating(const std::vector<int>& ratings) {
         }
         int rating_sum = std::accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
-  }
+}
 
 SearchServer:: QueryWord SearchServer:: ParseQueryWord(const std::string& text) const {
         if (text.empty()) {
@@ -107,7 +107,7 @@ SearchServer:: QueryWord SearchServer:: ParseQueryWord(const std::string& text) 
         }
 
         return {word, is_minus, IsStopWord(word)};
-  }
+}
 
 SearchServer:: Query SearchServer:: ParseQuery(const std::string& text) const {
         Query result;
@@ -122,9 +122,9 @@ SearchServer:: Query SearchServer:: ParseQuery(const std::string& text) const {
             }
         }
         return result;
-  }
+}
 
     // Existence required
 double SearchServer:: ComputeWordInverseDocumentFreq(const std::string& word) const {
        return log(GetDocumentCount() * 1.0 /                                             word_to_document_freqs_.at(word).size());
-  }
+}
