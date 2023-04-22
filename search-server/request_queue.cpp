aa54ac64a000e9cea2bb879632246f2463783a33
Ilya <119296699+JonnyRogue@users.noreply.github.com>
2023-04-22 18:06:13 +0300

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,13 +1,13 @@
 #include "request_queue.h"
 
 
-std::vector<Document> RequestQueue:: AddFindRequest(const std::string& raw_query, DocumentStatus status) {
+std::vector<Document> RequestQueue::AddFindRequest(std::string_view raw_query, DocumentStatus status) {
         // напишите реализацию
     return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
         return document_status == status;
     });
 }
-std::vector<Document>RequestQueue:: AddFindRequest(const std::string& raw_query) {
+std::vector<Document> RequestQueue::AddFindRequest(std::string_view raw_query) {
         // напишите реализацию
     return AddFindRequest (raw_query, DocumentStatus::ACTUAL);
 }
